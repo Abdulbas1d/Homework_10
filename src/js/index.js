282cae3c8f541581c12698df5763fa8e424ad9d8
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-05-18 13:49:02 +0500

    Writing many codes

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
 const themeToggle = document.getElementById("theme-toggle");
 const html = document.documentElement;
 
+if (localStorage.getItem("theme")) {
+    html.setAttribute("data-theme", localStorage.getItem("theme"));
+    themeToggle.checked = html.dataset.theme == 'dracula' ?true : false;
+}
+
 themeToggle.addEventListener("input", () => {
     html.setAttribute(
         "data-theme", 
         html.dataset.theme == "winter" ? "dracula" : "winter"
     );
+    localStorage.setItem("theme", html.dataset.theme)
 });
